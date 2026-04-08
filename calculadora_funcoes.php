ea8b8e28704113ebfca7e232c16b06e16a0b181c
lndsilva <lndsilva@hotmail.com>
2026-04-07 21:39:48 -0300

    trabalhando com funções em php

--- calculadora_funcoes.php
+++ calculadora_funcoes.php
@@ -5,13 +5,17 @@
 		return ($num1 + $num2);
 	}
 	function subtrairValor($num1,$num2){
-		return ($num1 + $num2);
+		return ($num1 - $num2);
 	}
 	function multiplicarValor($num1,$num2){
-		return ($num1 + $num2);
+		return ($num1 * $num2);
 	}
 	function dividirValor($num1,$num2){
-		return ($num1 + $num2);
+		if ($num2 == 0) {
+			return "impossível divisão por 0";
+		}else{
+			return ($num1 / $num2);
+		}
 	}
 
 	function operacao($operacao)
@@ -20,29 +24,29 @@
 	}
 
 	$num1 = 10.50;
-	$num2 = 20.0;
-	$oper = 1;
+	$num2 = 0.0;
+	$oper = 4;
 
 	switch ($oper) {
 		case 1:
 			$resp = somarValor($num1,$num2);
-			operacao("soma");
+			$op = operacao("soma");
 			break;
 		case 2:
 			$resp = subtrairValor($num1,$num2);
-			operacao("subtração");
+			$op = operacao("subtração");
 			break;
 		case 3:
 			$resp = multiplicarValor($num1,$num2);
-			operacao("multiplicação");
+			$op = operacao("multiplicação");
 			break;
 		case 4:
 			$resp= dividirValor($num1,$num2);
-			operacao("divisão");
+			$op = operacao("divisão");
 			break;		
 	}
 
-	echo "A ".operacao."de $num1 e $num2 é: $resp";
+	echo "A $op de $num1 e $num2 é: $resp";
 
 
 
